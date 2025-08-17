0800e7b9e8da00de2eb63fbd3fc99a432b59cd47
luka gugushvili <gugushvili58@gmail.com>
2025-08-17 17:05:38 +0400

    chore: set up Docker and MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017',
+    ),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('tiny'));
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
